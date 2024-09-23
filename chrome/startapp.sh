acde4fb129a78da615e5e87e4534c4bba9e60b78
Cyrill Raccaud <cyrill.raccaud@aposphere.com>
2024-09-23 17:08:30 +0200

    add no sandbox param

--- chrome/startapp.sh
+++ chrome/startapp.sh
@@ -14,4 +14,4 @@ fi
 socat TCP-LISTEN:9223,fork TCP:127.0.0.1:9222 &
 
 # Start chromium
-exec /usr/bin/chromium-browser --disable-software-rasterizer --disable-dev-shm-usage --disable-gpu --ignore-gpu-blocklist --no-first-run --password-store=basic --user-data-dir=/tmp/chromium-profile --start-maximized --remote-debugging-port=9222 --remote-debugging-address=0.0.0.0
+exec /usr/bin/chromium-browser --disable-software-rasterizer --disable-dev-shm-usage --disable-gpu --ignore-gpu-blocklist --no-first-run --no-sandbox --password-store=basic --user-data-dir=/tmp/chromium-profile --start-maximized --remote-debugging-port=9222 --remote-debugging-address=0.0.0.0
